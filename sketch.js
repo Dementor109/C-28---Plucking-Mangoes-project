ee3dd989237b3e1c5cb049b71fcab0e96ae5946e
Dementor109 <84393978+Dementor109@users.noreply.github.com>
2021-08-17 11:21:22 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-const Constraint=Matter.Constraint;
+const Constraint= Matter.Constraint;
 
 var tree, stone,ground;
 var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8,mango9,mango10,mango11,mango12;
